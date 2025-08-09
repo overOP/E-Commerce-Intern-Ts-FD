fcfee3ba3756e50d3f8c8abb682779e93dd300c4
overOP <cgpradeepop@gmail.com>
2025-08-09 17:09:05 +0545

    fix

--- src/Routes/Route.tsx
+++ src/Routes/Route.tsx
@@ -1,46 +1,3 @@
-// import { createBrowserRouter } from "react-router";
-// import Layout from "../Layout/Layout";
-// import Home from "../Pages/Home";
-// import Cart from "../Components/Cart";
-// import ShopMain from "../Components/Shopnow/ShopMain"
-// import Shop from "../Pages/Shop";
-// import Contact from "../Pages/Contact";
-
-// // Account section components
-// import Account from "../Layout/Account";
-// import Main from "../Pages/account/Main";
-// import Address from "../Pages/account/Address";
-// import Orders from "../Pages/account/Orders";
-// import Wishlist from "../Pages/account/Wishlist";
-// import Signup from "../Layout/Signup";
-
-// export const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Layout />,
-//     children: [
-//       { path: "/", element: <Home /> },
-//       { path: "/cart", element: <Cart /> },
-//       {
-//         path: "/account",
-//         element: <Account />,
-//         children: [
-//           { index: true, element: <Main /> }, 
-//           { path: "address", element: <Address /> },
-//           { path: "orders", element: <Orders /> },
-//           { path: "wishlist", element: <Wishlist /> }
-//         ]
-//       },
-//       { path: "/shopmain/:id", element: <ShopMain/> },
-//       { path: "/shop", element: <Shop /> },
-//       { path: "/contact", element: <Contact /> },
-//       { path: "signup", element: <Signup/>}
-//     ]
-//   }
-// ]);
-
-
-
 import { createBrowserRouter, Navigate } from "react-router";
 import Layout from "../Layout/Layout";
 import Home from "../Pages/Home";
@@ -89,7 +46,7 @@ export const router = createBrowserRouter([
       {
         element: <Layout />,
         children: [
-          { path: "/", element: <Home /> },  // 👈 main home page at "/"
+          { path: "/", element: <Home /> },
           { path: "/cart", element: <Cart /> },
           { path: "/shopmain/:id", element: <ShopMain /> },
           { path: "/shop", element: <Shop /> },
